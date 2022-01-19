cfd1e3240db6e11c024009bac4d9f16d32077413
longwei <longwei27@huawei.com>
2022-01-19 16:23:13 +0800

    fix packtools for new hap contructor
    Signed-off-by: longwei <longwei27@huawei.com>
    Change-Id: Iaf657f19405204c48d0a060a967041a952b2c5c0

---
+++ adapter/ohos/Version.java
@@ -0,0 +1,20 @@
+package ohos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Version info.
+ *
+ */
+public class Version {
+    /**
+     * Indicates the versionCode of version.
+     */
+    public int versionCode = -1;
+
+    /**
+     * Indicates the versionName of version.
+     */
+    public String versionName = "";
+}
