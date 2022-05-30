3800f2b7e1064b837fa66294995c202f2b42e655
longwei <longwei27@huawei.com>
2022-05-30 17:23:41 +0800

    fix verify rule when pack app
    Signed-off-by: longwei <longwei27@huawei.com>
    Change-Id: Id9dee4e7f34b2929344af0d0366c71663250e137

---
+++ adapter/ohos/APIVersion.java
@@ -0,0 +1,16 @@
+package ohos;
+
+public class APIVersion {
+    /**
+     * Indicates the compatibleApiVersion of app.
+     */
+    public int compatibleApiVersion = -1;
+    /**
+     * Indicates the compatibleApiVersion of app.
+     */
+    public int targetApiVersion = -1;
+    /**
+     * Indicates the releaseType of app.
+     */
+    public String releaseType = "";
+}
