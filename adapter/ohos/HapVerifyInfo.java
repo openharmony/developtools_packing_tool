cdcb9930271739476a861634f05b4576b19af045
longwei <longwei27@huawei.com>
2022-08-06 15:40:05 +0800

    fix codex
    Signed-off-by: longwei <longwei27@huawei.com>
    Change-Id: Ifd453b8b46507e6f1e7462168db75c6cb8742bf7

--- adapter/ohos/HapVerifyInfo.java
+++ adapter/ohos/HapVerifyInfo.java
@@ -23,61 +23,199 @@ class HapVerifyInfo {
     /**
      * Indicates the bundleName of module.
      */
-    public String bundleName = "";
+    private String bundleName = "";
+
     /**
      * Indicates the vendor of module.
      */
-    public String vendor = "";
+    private String vendor = "";
+
     /**
      * Indicates the version of module.
      */
-    public Version version = new Version();
+    private Version version = new Version();
+
     /**
      * Indicates the apiVersion of module.
      */
-    public ModuleApiVersion apiVersion = new ModuleApiVersion();
+    private ModuleApiVersion apiVersion = new ModuleApiVersion();
+
     /**
      * Indicates the moduleName of module.
      */
-    public String moduleName = "";
+    private String moduleName = "";
+
     /**
      * Indicates the package name of module.
      */
-    public String packageName = "";
+    private String packageName = "";
+
     /**
      * Indicates the ability names of module.
      */
-    public List<String> abilityNames = new ArrayList<>();
+    private List<String> abilityNames = new ArrayList<>();
+
     /**
      * Indicates the distrofilter of module.
      */
-    public DistroFilter distroFilter = new DistroFilter();
+    private DistroFilter distroFilter = new DistroFilter();
+
     /**
      * Indicates the deviceType of module.
      */
-    public List<String> deviceType = new ArrayList<>();
+    private List<String> deviceType = new ArrayList<>();
+
     /**
      * Indicates is stage of module.
      */
-    public boolean isStageModule = true;
+    private boolean isStageModule = true;
+
     /**
      * Indicates is entry of module.
      */
-    public boolean isEntry = false;
+    private boolean isEntry = false;
+
     /**
      * Indicates is installationFree of module.
      */
-    public boolean installationFree = false;
+    private boolean isInstallationFree = false;
+
     /**
      * Indicates dependency of module.
      */
-    public List<String> dependencies = new ArrayList<>();
+    private List<String> dependencies = new ArrayList<>();
+
     /**
      * Indicates is config.json string or module.json string of module.
      */
-    public String profileStr = "";
+    private String profileStr = "";
+
     /**
      * Indicates is file in profile of module.
      */
-    public HashMap<String, String> resourceMap = new HashMap<>();
+    private HashMap<String, String> resourceMap = new HashMap<>();
+
+    public String getBundleName() {
+        return bundleName;
+    }
+
+    public void setBundleName(String bundleName) {
+        this.bundleName = bundleName;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public Version getVersion() {
+        return version;
+    }
+
+    public void setVersion(Version version) {
+        this.version = version;
+    }
+
+    public ModuleApiVersion getApiVersion() {
+        return apiVersion;
+    }
+
+    public void setApiVersion(ModuleApiVersion apiVersion) {
+        this.apiVersion = apiVersion;
+    }
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public List<String> getAbilityNames() {
+        return abilityNames;
+    }
+
+    public void setAbilityNames(List<String> abilityNames) {
+        this.abilityNames = abilityNames;
+    }
+
+    public void addAbilityNames(List<String> nameList) {
+        this.abilityNames.addAll(nameList);
+    }
+
+    public DistroFilter getDistroFilter() {
+        return distroFilter;
+    }
+
+    public void setDistroFilter(DistroFilter distroFilter) {
+        this.distroFilter = distroFilter;
+    }
+
+    public List<String> getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(List<String> deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public boolean isStageModule() {
+        return isStageModule;
+    }
+
+    public void setStageModule(boolean stageModule) {
+        isStageModule = stageModule;
+    }
+
+    public boolean isEntry() {
+        return isEntry;
+    }
+
+    public void setEntry(boolean entry) {
+        isEntry = entry;
+    }
+
+    public boolean isInstallationFree() {
+        return isInstallationFree;
+    }
+
+    public void setInstallationFree(boolean installationFree) {
+        this.isInstallationFree = installationFree;
+    }
+
+    public List<String> getDependencies() {
+        return dependencies;
+    }
+
+    public void setDependencies(List<String> dependencies) {
+        this.dependencies = dependencies;
+    }
+
+    public String getProfileStr() {
+        return profileStr;
+    }
+
+    public void setProfileStr(String profileStr) {
+        this.profileStr = profileStr;
+    }
+
+    public HashMap<String, String> getResourceMap() {
+        return resourceMap;
+    }
+
+    public void setResourceMap(HashMap<String, String> resourceMap) {
+        this.resourceMap = resourceMap;
+    }
 }
