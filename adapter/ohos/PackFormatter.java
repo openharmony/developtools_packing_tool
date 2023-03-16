5c2a1b6beb6f915f7e240b8011a1b8d6dc3fad76
耿文广 <gengwenguang@huawei.com>
2023-03-16 06:04:57 +0000

    update adapter/ohos/PackFormatter.java.
    Signed-off-by: 耿文广 <gengwenguang@huawei.com>

--- adapter/ohos/PackFormatter.java
+++ adapter/ohos/PackFormatter.java
@@ -25,6 +25,7 @@ import java.util.logging.LogRecord;
  *
  */
 public class PackFormatter extends Formatter {
+    @Override
     /**
      * format builder
      *
@@ -41,5 +42,4 @@ public class PackFormatter extends Formatter {
 
     private static final int LOG_MAX_LIMIT = 1000;
     private static final DateFormat DF = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss.SSS");
-    @Override
 }
