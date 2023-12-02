acc23985cf79ae5edd7d7c7f6cc956d9e03271af
hupeixi <hupeixi1@huawei.com>
2023-12-02 19:41:21 +0800

    扫描工具编译文件
    Signed-off-by: hupeixi <hupeixi1@huawei.com>

--- adapter/ohos/CommandParser.java
+++ adapter/ohos/CommandParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -304,11 +304,11 @@ public class CommandParser {
             return true;
         });
         commandFuncs.put(STAT_DUPLICATE, entry -> {
-            entry.getKey().setStatDuplicate(entry.getValue());
+            entry.getKey().setStatDuplicate(Boolean.parseBoolean(entry.getValue()));
             return true;
         });
         commandFuncs.put(STAT_SUFFIX, entry -> {
-            entry.getKey().setStatSuffix(entry.getValue());
+            entry.getKey().setStatSuffix(Boolean.parseBoolean(entry.getValue()));
             return true;
         });
         commandFuncs.put(STAT_FILE_SIZE, entry -> {
