0d12e43ebd1a6a1980c6d173d51768d382dd9dc1
18710539902 <sunxuhui7@huawei.com>
2024-08-15 11:15:41 +0800

    打包日志鸿蒙化1-新增packing_tool基本框架、日志等
    Signed-off-by: 18710539902 <sunxuhui7@huawei.com>
    Change-Id: Idb517cd8de73b655eac877a40cf240e1021b29b2

---
+++ packing_tool/frameworks/include/log.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DEVELOPTOOLS_PACKING_TOOL_APT_FRAMEWORKS_INCLUDE_LOG_H
+#define DEVELOPTOOLS_PACKING_TOOL_APT_FRAMEWORKS_INCLUDE_LOG_H
+
+namespace OHOS {
+namespace AppPackingTool {
+
+enum LOG_LEVEL {
+    LOG_LEVEL_DEBUG = 0,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_FATAL
+};
+
+void Log(char *file, char *func, int line, int level, char *format, ...);
+
+#define LOGD(format, ...) Log((char *)__FILE__, (char *)__func__, __LINE__, LOG_LEVEL_DEBUG, \
+    (char *)format, ##__VA_ARGS__)
+#define LOGI(format, ...) Log((char *)__FILE__, (char *)__func__, __LINE__, LOG_LEVEL_INFO, \
+    (char *)format, ##__VA_ARGS__)
+#define LOGW(format, ...) Log((char *)__FILE__, (char *)__func__, __LINE__, LOG_LEVEL_WARN, \
+    (char *)format, ##__VA_ARGS__)
+#define LOGE(format, ...) Log((char *)__FILE__, (char *)__func__, __LINE__, LOG_LEVEL_ERROR, \
+    (char *)format, ##__VA_ARGS__)
+#define LOGF(format, ...) Log((char *)__FILE__, (char *)__func__, __LINE__, LOG_LEVEL_FATAL, \
+    (char *)format, ##__VA_ARGS__)
+
+}  // namespace AppPackingTool
+}  // namespace OHOS
+
+#endif  // DEVELOPTOOLS_PACKING_TOOL_APT_FRAMEWORKS_INCLUDE_ZIP_UTILS_H
---
+++ packing_tool/frameworks/src/hap_packager.cpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include <regex>
+#include <string>
+
+#include "hap_packager.h"
+#include "constants.h"
+#include "contrib/minizip/zip.h"
+#include "nlohmann/json.hpp"
+#include "packager.h"
+
+namespace OHOS {
+namespace AppPackingTool {
+namespace {}
+
+HapPackager::HapPackager(const std::map<std::string, std::string> &parameterMap, std::string &resultReceiver)
+    : Packager(parameterMap, resultReceiver)
+{}
+
+int HapPackager::InitAllowedParam()
+{
+    allowedParameters_ = {
+        {}
+    };
+    return ERR_OK;
+}
+
+int HapPackager::PreProcess()
+{
+    return ERR_OK;
+}
+
+int HapPackager::Process()
+{
+    std::string outPath = parameterMap_.at(Constants::PARAM_OUT_PATH);
+    zipWrapper_.Open(outPath);
+    if (!zipWrapper_.IsOpen()) {
+        std::cout << "HapPackager::Process: zipWrapper Open failed!" << std::endl;
+        return ERR_INVALID_VALUE;
+    }
+    std::map<std::string, std::string>::const_iterator it = parameterMap_.find(Constants::PARAM_JSON_PATH);
+    if (it != parameterMap_.end()) {
+        if (ParseJsonFile(moduleJson, it->second)) {
+            if (zipWrapper_.WriteStringToZip(moduleJson.dump(), Constants::MODULE_JSON) !=
+                ZipErrCode::ZIP_ERR_SUCCESS) {
+                std::cout << "HapPackager::Process: zipWrapper WriteStringToZip failed!" << std::endl;
+                return ERR_INVALID_VALUE;
+            }
+        }
+    }
+    it = parameterMap_.find(Constants::PARAM_LIB_PATH);
+    if (it != parameterMap_.end()) {
+        zipWrapper_.AddFileOrDirectoryToZip(it->second, Constants::LIB_PATH);
+    }
+    it = parameterMap_.find(Constants::PARAM_RESOURCES_PATH);
+    if (it != parameterMap_.end()) {
+        zipWrapper_.AddFileOrDirectoryToZip(it->second, Constants::RESOURCES_PATH);
+    }
+    it = parameterMap_.find(Constants::PARAM_INDEX_PATH);
+    if (it != parameterMap_.end()) {
+        zipWrapper_.AddFileOrDirectoryToZip(it->second, Constants::RESOURCES_INDEX);
+    }
+    it = parameterMap_.find(Constants::PARAM_PACK_INFO_PATH);
+    if (it != parameterMap_.end()) {
+        zipWrapper_.AddFileOrDirectoryToZip(it->second, Constants::PACK_INFO);
+    }
+    it = parameterMap_.find(Constants::PARAM_ETS_PATH);
+    if (it != parameterMap_.end()) {
+        zipWrapper_.AddFileOrDirectoryToZip(it->second, Constants::ETS_PATH);
+    }
+    it = parameterMap_.find(Constants::PARAM_RPCID_PATH);
+    if (it != parameterMap_.end()) {
+        zipWrapper_.AddFileOrDirectoryToZip(it->second, Constants::RPCID_SC);
+    }
+    it = parameterMap_.find(Constants::PARAM_PKG_CONTEXT_PATH);
+    if (it != parameterMap_.end()) {
+        zipWrapper_.AddFileOrDirectoryToZip(it->second, Constants::PKG_CONTEXT_JSON);
+    }
+    zipWrapper_.Close();
+    return ERR_OK;
+}
+
+int HapPackager::PostProcess()
+{
+    return ERR_OK;
+}
+
+} // namespace AppPackingTool
+} // namespace OHOS
