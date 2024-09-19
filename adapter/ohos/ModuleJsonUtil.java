1cc881e0c0b3c49509e5e1d6b82fed2455e19363
z30034863 <zhangfengzhuang@huawei.com>
2024-09-19 21:48:47 +0800

    1、har/hsp支持abilities 2、系统级hsp和应用间hsp若包含abilities,打包时报错
    Signed-off-by: z30034863 <zhangfengzhuang@huawei.com>

--- adapter/ohos/ModuleJsonUtil.java
+++ adapter/ohos/ModuleJsonUtil.java
@@ -21,10 +21,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONException;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class ModuleJsonUtil {
     private static final String APP = "app";
@@ -1797,6 +1794,22 @@ class ModuleJsonUtil {
             return false;
         }
         return true;
+    }
+
+    /**
+     * check whether hsp module has abilities or extensionAbilities.
+     *
+     * @param utility pack info.
+     * @return Returns true if jsonString is valid.
+     */
+    public static boolean checkHspHasAbilities(Utility utility) throws BundleException {
+        Optional<String> optional = FileUtils.getFileContent(utility.getJsonPath());
+        String jsonString = optional.orElseThrow(new BundleException("checkHspHasAbilities(*) -> jsonPath is null"));
+        boolean result = parseModuleType(jsonString).equals(ModuleJsonUtil.SHARED) && !parseAbilityNames(jsonString).isEmpty();
+        if(result) {
+            LOG.info("checkHspHasAbilities(*) -> hsp module has abilities or extensionAbilities");
+        }
+        return result;
     }
 
     /**
