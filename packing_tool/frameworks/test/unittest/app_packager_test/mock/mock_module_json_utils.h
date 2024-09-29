311dbcb54ecc7a2f98baa042948b44382e48dd36
sunjiakun <sunjiakun5@huawei.com>
2024-09-29 14:45:22 +0800

    补充tdd测试用例
    Signed-off-by: sunjiakun <sunjiakun5@huawei.com>

---
+++ packing_tool/frameworks/test/unittest/app_packager_test/mock/mock_module_json_utils.h
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MOCK_MODULE_JSON_UTILS_H
+#define MOCK_MODULE_JSON_UTILS_H
+
+#include <vector>
+#include "hap_verify_info.h"
+
+namespace OHOS {
+
+using namespace OHOS::AppPackingTool;
+using ResultSeries = std::vector<std::pair<bool, HapVerifyInfo>>;
+
+class MockModuleJsonUtils {
+public:
+    static bool mockGetStageHapVerifyInfo_;
+    static bool mockGetFaHapVerifyInfo_;
+    static bool mockCheckHapsIsValid_;
+    static bool mockIsModuleHap_;
+    static bool mockCheckHapsIsValidResult_;
+    static bool mockIsModuleHapResult_;
+    static ResultSeries mockGetStageHapVerifyInfoResultSeries_;
+    static ResultSeries::const_iterator mockGetStageHapVerifyInfoResultSeriesIter_;
+    static ResultSeries mockGetFaHapVerifyInfoResultSeries_;
+    static ResultSeries::const_iterator mockGetFaHapVerifyInfoResultSeriesIter_;
+
+public:
+    static void Reset()
+    {
+        mockGetStageHapVerifyInfo_ = false;
+        mockGetFaHapVerifyInfo_ = false;
+        mockCheckHapsIsValid_ = false;
+        mockIsModuleHap_ = false;
+        mockCheckHapsIsValidResult_ = false;
+        mockIsModuleHapResult_ = false;
+        mockGetStageHapVerifyInfoResultSeries_.clear();
+        mockGetStageHapVerifyInfoResultSeriesIter_ = mockGetStageHapVerifyInfoResultSeries_.cbegin();
+        mockGetFaHapVerifyInfoResultSeries_.clear();
+        mockGetFaHapVerifyInfoResultSeriesIter_ = mockGetStageHapVerifyInfoResultSeries_.cbegin();
+    }
+
+    static void MockGetStageHapVerifyInfo(const ResultSeries& resultSeries)
+    {
+        mockGetStageHapVerifyInfo_ = true;
+        mockGetStageHapVerifyInfoResultSeries_ = resultSeries;
+        mockGetStageHapVerifyInfoResultSeriesIter_ = mockGetStageHapVerifyInfoResultSeries_.cbegin();
+    }
+
+    static void MockGetStageHapVerifyInfo(const bool result, const HapVerifyInfo& hapVerifyInfo)
+    {
+        ResultSeries series;
+        series.emplace_back(result, hapVerifyInfo);
+        MockGetStageHapVerifyInfo(series);
+    }
+
+    static void MockGetFaHapVerifyInfo(const ResultSeries& resultSeries)
+    {
+        mockGetFaHapVerifyInfo_ = true;
+        mockGetFaHapVerifyInfoResultSeries_ = resultSeries;
+        mockGetFaHapVerifyInfoResultSeriesIter_ = mockGetStageHapVerifyInfoResultSeries_.cbegin();
+    }
+
+    static void MockGetFaHapVerifyInfo(const bool result, const HapVerifyInfo& hapVerifyInfo)
+    {
+        ResultSeries series;
+        series.emplace_back(result, hapVerifyInfo);
+        MockGetFaHapVerifyInfo(series);
+    }
+
+    static void MockCheckHapsIsValid(const bool result)
+    {
+        mockCheckHapsIsValid_ = true;
+        mockCheckHapsIsValidResult_ = result;
+    }
+
+    static void MockIsModuleHap(const bool result)
+    {
+        mockIsModuleHap_ = true;
+        mockIsModuleHapResult_ = result;
+    }
+};
+} // namespace OHOS
+#endif // MOCK_MODULE_JSON_UTILS_H
