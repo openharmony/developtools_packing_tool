c31d1fecd3798b3d2a4f26ad5551302d15c8ff21
sunjiakun <sunjiakun5@huawei.com>
2024-10-09 16:36:41 +0800

    add tdd
    Signed-off-by: sunjiakun <sunjiakun5@huawei.com>

--- packing_tool/frameworks/test/unittest/hsp_packager_test/hsp_packager_test.cpp
+++ packing_tool/frameworks/test/unittest/hsp_packager_test/hsp_packager_test.cpp
@@ -15,6 +15,7 @@
 
 #include <gtest/gtest.h>
 #include <cstdlib>
+#include <fstream>
 #include <string>
 
 #define private public
@@ -34,6 +35,25 @@ const std::string PACK_INFO_PATH = "/data/test/resource/packingtool/test_file/pa
 const std::string ETS_PATH = "/data/test/resource/packingtool/test_file/ets";
 const std::string RESOURCES_PATH = "/data/test/resource/packingtool/test_file/resources";
 const std::string JSON_PATH = "/data/test/resource/packingtool/test_file/module.json";
+const std::string TEST_PATH = "/data/test/resources.index";
+const std::string MODULE_JSON_TEST_STRING = "{"
+    "\"app\": {"
+        "\"apiVersion\": {"
+        "}"
+    "},"
+    "\"module\": {"
+        "\"requestPermissions\": true,"
+        "\"proxyData\": ["
+            "{"
+                 "\"uri\": \"test_uri1\""
+            "},"
+            "{"
+                 "\"uri\": \"test_uri2\""
+            "}"
+        "],"
+        "\"generateBuildHash\": true"
+    "}"
+"}";
 }
 
 class HspPackagerTest : public testing::Test {
@@ -54,9 +74,15 @@ void HspPackagerTest::SetUpTestCase() {}
 
 void HspPackagerTest::TearDownTestCase() {}
 
-void HspPackagerTest::SetUp() {}
+void HspPackagerTest::SetUp()
+{
+    system("mkdir -p /data/test/resource/packingtool/test_file");
+}
 
-void HspPackagerTest::TearDown() {}
+void HspPackagerTest::TearDown()
+{
+    system("rm -rf /data/test/resource");
+}
 
 /*
  * @tc.name: hspPackager_0100
@@ -92,5 +118,426 @@ HWTEST_F(HspPackagerTest, hspPackager_0100, Function | MediumTest | Level1)
     std::string cmd = {"rm -f "};
     cmd += OUT_PATH;
     system(cmd.c_str());
+}
+
+/*
+ * @tc.name: IsVerifyValidInHspMode_0200
+ * @tc.desc: IsVerifyValidInHspMode.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, IsVerifyValidInHspMode_0200, Function | MediumTest | Level1)
+{
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap = {
+        {OHOS::AppPackingTool::Constants::PARAM_ETS_PATH, ETS_PATH},
+    };
+
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    EXPECT_FALSE(hspPackager.IsVerifyValidInHspMode());
+}
+
+/*
+ * @tc.name: CompressHsp_0300
+ * @tc.desc: CompressHsp.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, CompressHsp_0300, Function | MediumTest | Level1)
+{
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap;
+
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    EXPECT_FALSE(hspPackager.CompressHsp());
+
+    system("touch /data/test/resource/packingtool/test_file/module.json");
+    hspPackager.jsonPath_ = JSON_PATH;
+    EXPECT_FALSE(hspPackager.CompressHsp());
+    system("rm -f /data/test/resource/packingtool/test_file/module.json");
+}
+
+/*
+ * @tc.name: CompressHsp_0400
+ * @tc.desc: CompressHsp.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, CompressHsp_0400, Function | MediumTest | Level1)
+{
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap;
+
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    system("touch /data/test/resource/packingtool/test_file/module.json");
+    system("chmod 777 /data/test/resource/packingtool/test_file/module.json");
+    std::ofstream file("/data/test/resource/packingtool/test_file/module.json");
+    if (file.is_open()) {
+        file << MODULE_JSON_TEST_STRING;
+        file.close();
+    }
+    hspPackager.jsonPath_ = JSON_PATH;
+    EXPECT_FALSE(hspPackager.CompressHsp());
+    system("rm -f /data/test/resource/packingtool/test_file/module.json");
+}
+
+/*
+ * @tc.name: CompressHspMode_0500
+ * @tc.desc: CompressHspMode.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, CompressHspMode_0500, Function | MediumTest | Level1)
+{
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap1 = {
+        {OHOS::AppPackingTool::Constants::PARAM_OUT_PATH, ""},
+    };
+
+    OHOS::AppPackingTool::HspPackager hspPackager1(parameterMap1, resultReceiver);
+    EXPECT_FALSE(hspPackager1.CompressHspMode(JSON_PATH));
+
+    std::map<std::string, std::string> parameterMap = {
+        {OHOS::AppPackingTool::Constants::PARAM_OUT_PATH, OUT_PATH},
+        {OHOS::AppPackingTool::Constants::PARAM_INDEX_PATH, TEST_PATH},
+    };
+    system("touch /data/test/library-default-unsigned.hsp");
+    system("touch /data/test/resources.index");
+    system("touch /data/test/resource/packingtool/test_file/module.json");
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    hspPackager.moduleJson_.ParseFromString(MODULE_JSON_TEST_STRING);
+    EXPECT_TRUE(hspPackager.CompressHspMode(JSON_PATH));
+    std::string jsonPath = "aa";
+    EXPECT_TRUE(hspPackager.CompressHspMode(jsonPath));
+    system("rm -f /data/test/library-default-unsigned.hsp");
+    system("rm -f /data/test/resource/packingtool/test_file/module.json");
+    system("rm -f /data/test/resources.index");
+}
+
+/*
+ * @tc.name: CompressHspModePartSecond_0600
+ * @tc.desc: CompressHspModePartSecond.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, CompressHspModePartSecond_0600, Function | MediumTest | Level1)
+{
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap = {
+        {OHOS::AppPackingTool::Constants::PARAM_LIB_PATH, TEST_PATH},
+    };
+    system("touch /data/test/resources.index");
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    EXPECT_FALSE(hspPackager.CompressHspModePartSecond(JSON_PATH));
+
+    std::string filepath = "/data/test/test";
+    system("mkdir /data/test/test");
+    std::map<std::string, std::string> parameterMap1 = {
+        {OHOS::AppPackingTool::Constants::PARAM_FILE_PATH, filepath}};
+    OHOS::AppPackingTool::HspPackager hspPackager1(parameterMap1, resultReceiver);
+    EXPECT_FALSE(hspPackager1.CompressHspModePartSecond(JSON_PATH));
+
+    std::map<std::string, std::string> parameterMap2 = {
+        {OHOS::AppPackingTool::Constants::PARAM_RESOURCES_PATH, TEST_PATH}};
+    OHOS::AppPackingTool::HspPackager hspPackager2(parameterMap2, resultReceiver);
+    EXPECT_TRUE(hspPackager2.CompressHspModePartSecond(JSON_PATH));
+    system("touch /data/test/resource/packingtool/test_file/module.json");
+    EXPECT_FALSE(hspPackager2.CompressHspModePartSecond(JSON_PATH));
+    system("rm -f /data/test/resources.index");
+    system("rm -f /data/test/resource/packingtool/test_file/module.json");
+    system("rm -rf /data/test/test");
+}
+
+/*
+ * @tc.name: CompressHspModePartThird_0700
+ * @tc.desc: CompressHspModePartThird.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, CompressHspModePartThird_0700, Function | MediumTest | Level1)
+{
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap = {
+        {OHOS::AppPackingTool::Constants::PARAM_BIN_PATH, TEST_PATH},
+    };
+    system("touch /data/test/resources.index");
+    std::list<std::string> deviceTypes = {"fitnessWatch"};
+
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    hspPackager.deviceTypes_ = deviceTypes;
+    EXPECT_FALSE(hspPackager.CompressHspModePartThird(JSON_PATH));
+
+    std::map<std::string, std::string> parameterMap1 = {
+        {OHOS::AppPackingTool::Constants::PARAM_JS_PATH, TEST_PATH},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager1(parameterMap1, resultReceiver);
+    EXPECT_TRUE(hspPackager1.CompressHspModePartThird(JSON_PATH));
+    system("touch /data/test/resource/packingtool/test_file/module.json");
+    EXPECT_FALSE(hspPackager1.CompressHspModePartThird(JSON_PATH));
+    system("rm -f /data/test/resource/packingtool/test_file/module.json");
+
+    std::map<std::string, std::string> parameterMap2 = {
+        {OHOS::AppPackingTool::Constants::PARAM_JS_PATH, TEST_PATH},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager2(parameterMap2, resultReceiver);
+    EXPECT_TRUE(hspPackager2.CompressHspModePartThird(JSON_PATH));
+    system("touch /data/test/resource/packingtool/test_file/module.json");
+    EXPECT_FALSE(hspPackager2.CompressHspModePartThird(JSON_PATH));
+    system("rm -f /data/test/resource/packingtool/test_file/module.json");
+    system("rm -f /data/test/resources.index");
+}
+
+/*
+ * @tc.name: CompressHspModePartFourth_0800
+ * @tc.desc: CompressHspModePartFourth.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, CompressHspModePartFourth_0800, Function | MediumTest | Level1)
+{
+    system("touch /data/test/resources.index");
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap = {
+        {OHOS::AppPackingTool::Constants::PARAM_BIN_PATH, TEST_PATH},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    EXPECT_FALSE(hspPackager.CompressHspModePartFourth());
+
+    std::map<std::string, std::string> parameterMap1 = {
+        {OHOS::AppPackingTool::Constants::PARAM_PACK_INFO_PATH, TEST_PATH},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager1(parameterMap1, resultReceiver);
+    EXPECT_FALSE(hspPackager1.CompressHspModePartFourth());
+
+    std::map<std::string, std::string> parameterMap2 = {
+        {OHOS::AppPackingTool::Constants::PARAM_PKG_CONTEXT_PATH, JSON_PATH},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager2(parameterMap2, resultReceiver);
+    hspPackager2.formatedDirList_.push_back(TEST_PATH);
+    EXPECT_FALSE(hspPackager2.CompressHspModePartFourth());
+    hspPackager2.formatedDirList_.clear();
+    EXPECT_FALSE(hspPackager2.CompressHspModePartFourth());
+    system("touch /data/test/resource/packingtool/test_file/module.json");
+    system("chmod 777 /data/test/resource/packingtool/test_file/module.json");
+    std::ofstream file("/data/test/resource/packingtool/test_file/module.json");
+    if (file.is_open())
+    {
+        file << MODULE_JSON_TEST_STRING;
+        file.close();
+    }
+    EXPECT_FALSE(hspPackager2.CompressHspModePartFourth());
+    system("rm -f /data/test/resources.index");
+    system("rm -f /data/test/resource/packingtool/test_file/module.json");
+}
+
+/*
+ * @tc.name: CompressHspModeMultiple_0900
+ * @tc.desc: CompressHspModeMultiple.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, CompressHspModeMultiple_0900, Function | MediumTest | Level1)
+{
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap = {
+        {OHOS::AppPackingTool::Constants::PARAM_MAPLE_SO_DIR, TEST_PATH},
+    };
+    system("touch /data/test/resources.index");
+
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    EXPECT_FALSE(hspPackager.CompressHspModeMultiple());
+
+    std::map<std::string, std::string> parameterMap1 = {
+        {OHOS::AppPackingTool::Constants::PARAM_SHAREDLIBS_PATH, TEST_PATH},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager1(parameterMap1, resultReceiver);
+    hspPackager1.formattedJarPathList_.push_back(TEST_PATH);
+    hspPackager1.formattedTxtPathList_.push_back(TEST_PATH);
+    EXPECT_FALSE(hspPackager1.CompressHspModeMultiple());
+    hspPackager1.formattedJarPathList_.clear();
+    EXPECT_FALSE(hspPackager1.CompressHspModeMultiple());
+    hspPackager1.formattedTxtPathList_.clear();
+    EXPECT_FALSE(hspPackager1.CompressHspModeMultiple());
+    system("rm -f /data/test/resources.index");
+}
+
+/*
+ * @tc.name: CompressHspModeMultiple_1000
+ * @tc.desc: CompressHspModeMultiple.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, CompressHspModeMultiple_1000, Function | MediumTest | Level1)
+{
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap = {
+        {OHOS::AppPackingTool::Constants::PARAM_MAPLE_SO_DIR, ""},
+        {OHOS::AppPackingTool::Constants::PARAM_SHAREDLIBS_PATH, ""},
+    };
+
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    EXPECT_TRUE(hspPackager.CompressHspModeMultiple());
+    hspPackager.formattedJarPathList_.push_back(TEST_PATH);
+    hspPackager.formattedTxtPathList_.push_back(TEST_PATH);
+    hspPackager.formattedSoPathList_.push_back(TEST_PATH);
+    EXPECT_TRUE(hspPackager.CompressHspModeMultiple());
+}
+
+/*
+ * @tc.name: CompressHspModePartFourth_1100
+ * @tc.desc: CompressHspModePartFourth.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, CompressHspModePartFourth_1100, Function | MediumTest | Level1)
+{
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap = {
+        {OHOS::AppPackingTool::Constants::PARAM_BIN_PATH, ""},
+        {OHOS::AppPackingTool::Constants::PARAM_PACK_INFO_PATH, ""},
+        {OHOS::AppPackingTool::Constants::PARAM_PKG_CONTEXT_PATH, ""},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    EXPECT_TRUE(hspPackager.CompressHspModePartFourth());
+}
+
+/*
+ * @tc.name: CompressHspModePartThird_1200
+ * @tc.desc: CompressHspModePartThird.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, CompressHspModePartThird_1200, Function | MediumTest | Level1)
+{
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap = {
+        {OHOS::AppPackingTool::Constants::PARAM_BIN_PATH, ""},
+        {OHOS::AppPackingTool::Constants::PARAM_JS_PATH, ""},
+        {OHOS::AppPackingTool::Constants::PARAM_JS_PATH, ""},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    EXPECT_TRUE(hspPackager.CompressHspModePartThird(JSON_PATH));
+}
+
+/*
+ * @tc.name: IsHspPathValid_1300
+ * @tc.desc: IsHspPathValid.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, IsHspPathValid_1300, Function | MediumTest | Level1)
+{
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap = {
+        {OHOS::AppPackingTool::Constants::PARAM_LIB_PATH, TEST_PATH},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    EXPECT_FALSE(hspPackager.IsHspPathValid());
+
+    std::map<std::string, std::string> parameterMap1 = {
+        {OHOS::AppPackingTool::Constants::PARAM_RES_PATH, TEST_PATH},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager1(parameterMap1, resultReceiver);
+    EXPECT_FALSE(hspPackager1.IsHspPathValid());
+
+    std::map<std::string, std::string> parameterMap2 = {
+        {OHOS::AppPackingTool::Constants::PARAM_RESOURCES_PATH, TEST_PATH},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager2(parameterMap2, resultReceiver);
+    EXPECT_FALSE(hspPackager2.IsHspPathValid());
+
+    std::map<std::string, std::string> parameterMap3 = {
+        {OHOS::AppPackingTool::Constants::PARAM_ASSETS_PATH, TEST_PATH},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager3(parameterMap3, resultReceiver);
+    EXPECT_FALSE(hspPackager3.IsHspPathValid());
+
+    std::map<std::string, std::string> parameterMap4 = {
+        {OHOS::AppPackingTool::Constants::PARAM_AP_PATH, TEST_PATH},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager4(parameterMap4, resultReceiver);
+    EXPECT_FALSE(hspPackager4.IsHspPathValid());
+
+    std::map<std::string, std::string> parameterMap5 = {
+        {OHOS::AppPackingTool::Constants::PARAM_AN_PATH, TEST_PATH},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager5(parameterMap5, resultReceiver);
+    EXPECT_FALSE(hspPackager5.IsHspPathValid());
+}
+
+/*
+ * @tc.name: CompressHspModePartSecond_1400
+ * @tc.desc: CompressHspModePartSecond.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, CompressHspModePartSecond_1400, Function | MediumTest | Level1)
+{
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap = {
+        {OHOS::AppPackingTool::Constants::PARAM_FILE_PATH, ""},
+        {OHOS::AppPackingTool::Constants::PARAM_RESOURCES_PATH, ""},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    EXPECT_TRUE(hspPackager.CompressHspModePartSecond(JSON_PATH));
+}
+
+/*
+ * @tc.name: IsVerifyValidInHspCommonMode_1500
+ * @tc.desc: IsVerifyValidInHspCommonMode.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(HspPackagerTest, IsVerifyValidInHspCommonMode_1500, Function | MediumTest | Level1)
+{
+    std::string resultReceiver;
+    std::map<std::string, std::string> parameterMap;
+    OHOS::AppPackingTool::HspPackager hspPackager(parameterMap, resultReceiver);
+    EXPECT_FALSE(hspPackager.IsVerifyValidInHspCommonMode());
+
+    std::map<std::string, std::string> parameterMap1 = {
+        {OHOS::AppPackingTool::Constants::PARAM_JSON_PATH, ""},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager1(parameterMap1, resultReceiver);
+    EXPECT_FALSE(hspPackager1.IsVerifyValidInHspCommonMode());
+
+    system("touch /data/test/resource/packingtool/test_file/module.json");
+    std::map<std::string, std::string> parameterMap2 = {
+        {OHOS::AppPackingTool::Constants::PARAM_JSON_PATH, JSON_PATH},
+        {OHOS::AppPackingTool::Constants::PARAM_DIR_LIST, ""},
+        {OHOS::AppPackingTool::Constants::PARAM_PROFILE_PATH, ""},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager2(parameterMap2, resultReceiver);
+    EXPECT_FALSE(hspPackager2.IsVerifyValidInHspCommonMode());
+
+    std::map<std::string, std::string> parameterMap3 = {
+        {OHOS::AppPackingTool::Constants::PARAM_JSON_PATH, JSON_PATH},
+        {OHOS::AppPackingTool::Constants::PARAM_DIR_LIST, ""},
+        {OHOS::AppPackingTool::Constants::PARAM_PROFILE_PATH, JSON_PATH},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager3(parameterMap3, resultReceiver);
+    EXPECT_FALSE(hspPackager3.IsVerifyValidInHspCommonMode());
+
+    std::string profilePath = "/data/test/CAPABILITY.profile";
+    system("touch /data/test/CAPABILITY.profile");
+    std::map<std::string, std::string> parameterMap4 = {
+        {OHOS::AppPackingTool::Constants::PARAM_JSON_PATH, JSON_PATH},
+        {OHOS::AppPackingTool::Constants::PARAM_DIR_LIST, ""},
+        {OHOS::AppPackingTool::Constants::PARAM_PROFILE_PATH, profilePath},
+        {OHOS::AppPackingTool::Constants::PARAM_PKG_CONTEXT_PATH, ""},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager4(parameterMap4, resultReceiver);
+    EXPECT_TRUE(hspPackager4.IsVerifyValidInHspCommonMode());
+
+    std::map<std::string, std::string> parameterMap5 = {
+        {OHOS::AppPackingTool::Constants::PARAM_JSON_PATH, JSON_PATH},
+        {OHOS::AppPackingTool::Constants::PARAM_DIR_LIST, ""},
+        {OHOS::AppPackingTool::Constants::PARAM_PROFILE_PATH, profilePath},
+        {OHOS::AppPackingTool::Constants::PARAM_PKG_CONTEXT_PATH, profilePath},
+    };
+    OHOS::AppPackingTool::HspPackager hspPackager5(parameterMap5, resultReceiver);
+    EXPECT_FALSE(hspPackager5.IsVerifyValidInHspCommonMode());
+    system("rm -f /data/test/resource/packingtool/test_file/module.json");
+    system("rm -f /data/test/CAPABILITY.profile");
 }
 } // namespace OHOS
