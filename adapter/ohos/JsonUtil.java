c3185aded0315acd063de30361286ba47b1c5e43
张丰壮 <zhangfengzhuang@huawei.com>
2024-10-22 02:01:58 +0000

    update adapter/ohos/JsonUtil.java.
    Signed-off-by: 张丰壮 <zhangfengzhuang@huawei.com>

--- adapter/ohos/JsonUtil.java
+++ adapter/ohos/JsonUtil.java
@@ -25,7 +25,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONException;
-import com.sun.xml.internal.ws.util.StringUtils;
 
 /**
  * Json Util.
