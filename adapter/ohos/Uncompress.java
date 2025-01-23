ad31c86589d038ace1032de1312bd4e42e66ef5b
张丰壮 <zhangfengzhuang@huawei.com>
2025-01-23 06:12:52 +0000

    update adapter/ohos/Uncompress.java.
    Signed-off-by: 张丰壮 <zhangfengzhuang@huawei.com>

--- adapter/ohos/Uncompress.java
+++ adapter/ohos/Uncompress.java
@@ -28,8 +28,6 @@ import java.io.InputStreamReader;
 
 import java.nio.charset.StandardCharsets;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
