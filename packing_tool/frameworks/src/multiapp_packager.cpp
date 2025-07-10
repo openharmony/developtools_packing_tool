d0b0fb87cf4fa42161248452cff72fb90aa92b49
z30034863 <zhangfengzhuang@huawei.com>
2025-07-10 21:56:21 +0800

    feature:Add atomicService module size limit param
    Signed-off-by: z30034863 <zhangfengzhuang@huawei.com>

--- packing_tool/frameworks/src/multiapp_packager.cpp
+++ packing_tool/frameworks/src/multiapp_packager.cpp
@@ -48,6 +48,9 @@ int32_t MultiAppPackager::PreProcess()
     if (!ret) {
         return ERR_INVALID_VALUE;
     }
+    if (!Packager::ParseAtomicServiceSizeLimit()) {
+        return ERR_INVALID_VALUE;
+    }
     return ERR_OK;
 }
 
